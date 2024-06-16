3155755bba022d03eb921681fe9d0a00dc4d5b1f
phonghdp <huaduphong5598@gmail.com>
2024-06-17 01:35:38 +0700

    add sonar config

--- week2/character_count.py
+++ week2/character_count.py
@@ -4,8 +4,10 @@ def character_count(str):
     for i in str:
         if i in dict_:
             dict_[i] += 1
-        else: dict_[i] = 1     
+        else:
+            dict_[i] = 1
     return dict_
 
-assert character_count ("Baby") == {'B': 1 , 'a': 1 , 'b': 1 , 'y': 1}
-print (character_count ('smiles'))
+
+assert character_count("Baby") == {'B': 1, 'a': 1, 'b': 1, 'y': 1}
+print(character_count('smiles'))
